1d8bbf9fb45cd73ac0880acef37e60e5a80b47f8
Valery-a <83373303+Valery-a@users.noreply.github.com>
2023-07-29 08:35:41 +0300

    adding crosshair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QPen
 
 class CrosshairApp(QMainWindow):
     def __init__(self):
@@ -11,6 +12,19 @@ class CrosshairApp(QMainWindow):
         self.setAttribute(Qt.WA_TranslucentBackground, True)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.WindowTransparentForInput)
         self.show()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(QPen(Qt.white, 2))
+
+        center_x = self.width() // 2
+        center_y = self.height() // 2
+        diameter = 50
+        half_line = diameter // 2
+
+        painter.drawLine(center_x, center_y - half_line, center_x, center_y + half_line)
+        painter.drawLine(center_x - half_line, center_y, center_x + half_line, center_y)
 
 def main():
     app = QApplication(sys.argv)
