07730aa6bd533d40c72f988b508187bb59286912
Valery-a <83373303+Valery-a@users.noreply.github.com>
2023-07-29 15:29:05 +0300

    adding config saves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import json
+import os
 from PyQt5.QtWidgets import QApplication, QFormLayout, QDialog, QFrame, QWidget, QLabel, QLineEdit, QSlider, QVBoxLayout, QHBoxLayout, QMainWindow, QPushButton
 from PyQt5.QtCore import Qt, pyqtSignal, QPoint, QRect
 from PyQt5.QtGui import QPainter, QPen, QColor
@@ -13,8 +15,8 @@ class CrosshairApp(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.diameter = 50
-        self.alpha = 0.5
+        self.diameter = 0
+        self.alpha = 1
         self.initUI()
         self.crosshair_color = QColor(Qt.white)
         self.crosshair_shape = "cross"
@@ -125,6 +127,7 @@ class OptionsMenu(QFrame):
         self.crosshair = crosshair
         self.is_night_theme = False
         self.initUI()
+        self.load_settings()
 
     def initUI(self):
         layout = QVBoxLayout(self)
@@ -136,6 +139,13 @@ class OptionsMenu(QFrame):
 
         self.title_bar = TitleBar(self)
         layout.addWidget(self.title_bar)
+
+        self.config_save_button = QPushButton("Config Save")
+        self.config_save_button.clicked.connect(self.save_settings)
+        self.config_save_button.setStyleSheet(
+            "QPushButton { background-color: #FF9900; color: #fff; border: none; border-radius: 5px; padding: 8px; min-width: 100px; } QPushButton:hover { background-color: #FF6600; }"
+        )
+        layout.addWidget(self.config_save_button)
 
         self.apply_button = QPushButton("Apply")
         self.apply_button.clicked.connect(self.apply_changes)
@@ -166,6 +176,8 @@ class OptionsMenu(QFrame):
         self.vertical_adjust_input.setStyleSheet(
             "QLineEdit { background-color: #f0f0f0; color: #333; border: 1px solid #ccc; border-radius: 5px; padding: 5px; }"
         )
+        layout.addWidget(vertical_adjust_label)
+        layout.addWidget(self.vertical_adjust_input)
 
         alpha_label = QLabel("Hide your bomba? (0 to 1)")
         self.alpha_slider = QSlider(Qt.Horizontal)
@@ -208,8 +220,60 @@ class OptionsMenu(QFrame):
             }
             """
         )
-
+        
         self.set_day_theme()
+
+    def save_settings(self):
+        # Gather the settings to save
+        settings = {
+            "diameter": self.crosshair.diameter,
+            "alpha": self.crosshair.alpha,
+            "crosshair_color": self.crosshair.crosshair_color.name(),
+            "crosshair_shape": self.crosshair.crosshair_shape,
+            "is_outline_border": self.crosshair.is_outline_border,
+            "night_theme": self.is_night_theme,  # Save the current theme state
+            "vertical_adjust_input": int(self.vertical_adjust_input.text()),  # Save the vertical adjust value
+        }
+
+        # Save the settings to a configuration file (e.g., config.json)
+        config_folder = "config"
+        os.makedirs(config_folder, exist_ok=True)  # Create the config folder if it doesn't exist
+        config_path = os.path.join(config_folder, "config.json")
+
+        with open(config_path, "w") as config_file:
+            json.dump(settings, config_file)
+
+    def load_settings(self):
+        # Load settings from the configuration file (if available)
+        config_folder = "config"
+        config_path = os.path.join(config_folder, "config.json")
+
+        try:
+            with open(config_path, "r") as config_file:
+                settings = json.load(config_file)
+
+            # Apply the loaded settings to the crosshair app
+            self.crosshair.update_diameter(settings.get("diameter", self.crosshair.diameter))
+            self.crosshair.alpha = settings.get("alpha", self.crosshair.alpha)
+            self.crosshair.set_transparency(self.crosshair.alpha)
+            self.crosshair.set_crosshair_color(settings.get("crosshair_color", self.crosshair.crosshair_color.name()))
+            self.crosshair.set_crosshair_shape(settings.get("crosshair_shape", self.crosshair.crosshair_shape))
+            self.crosshair.is_outline_border = settings.get("is_outline_border", self.crosshair.is_outline_border)
+            self.crosshair.new_window_y += settings.get("vertical_adjust_input", 0)  # Apply vertical adjust value
+            self.crosshair.setGeometry(self.crosshair.new_window_x, self.crosshair.new_window_y,
+                                       self.crosshair.window_width, self.crosshair.window_height)
+            self.crosshair.repaint()
+
+            # Load the theme setting and apply it
+            self.is_night_theme = settings.get("night_theme", False)
+            if self.is_night_theme:
+                self.set_night_theme()
+            else:
+                self.set_day_theme()
+
+        except (FileNotFoundError, json.JSONDecodeError):
+            # The config file doesn't exist or is corrupted, do nothing
+            pass
 
     def apply_changes(self):
         diameter_text = self.diameter_input.text()
