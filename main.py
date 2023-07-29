17ca45800c963bee6349a427083122e336a75452
Valery-a <83373303+Valery-a@users.noreply.github.com>
2023-07-29 12:35:54 +0300

    code refracturing + adding close all

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from PyQt5.QtCore import Qt, pyqtSignal, QPoint, QRect
 from PyQt5.QtGui import QPainter, QPen, QColor
 
 class CrosshairApp(QMainWindow):
+    closeRequested = pyqtSignal()
     diameterChanged = pyqtSignal(int)
+
+    def closeEvent(self, event):
+        self.closeRequested.emit()
+        super().closeEvent(event)
 
     def __init__(self):
         super().__init__()
@@ -114,82 +119,6 @@ class CrosshairApp(QMainWindow):
         except ValueError:
             pass
 
-class TitleBar(QWidget):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.setAutoFillBackground(True)
-
-        self.layout = QHBoxLayout(self)
-        self.layout.setContentsMargins(0, 0, 0, 0)
-        self.layout.setSpacing(0)
-
-        self.title_label = QLabel("BOMBA MENU", self)
-        self.title_label.setStyleSheet("color: black; font-size: 18px; padding-left: 10px;")
-
-        self.close_button = QPushButton("×", self)
-        self.close_button.setStyleSheet("""
-            QPushButton {
-                background-color: transparent;
-                color: black;
-                font-size: 20px;
-                width: 25px;
-                height: 25px;
-                border: none;
-                margin: 0;
-            }
-            QPushButton:hover {
-                border: 1px solid #ccc; 
-                border-radius: 5px;
-                background-color: #dc3545;
-            }
-        """)
-        self.close_button.clicked.connect(self.parent().close)
-
-        self.minimize_button = QPushButton("–", self)
-        self.minimize_button.setStyleSheet("""
-            QPushButton {
-                background-color: transparent;
-                color: black;
-                font-size: 20px;
-                width: 25px;
-                height: 25px;
-                border: none;
-                margin: 0;
-            }
-            QPushButton:hover {
-                border: 1px solid #ccc; 
-                border-radius: 5px;
-                background-color: #Ffa500
-;
-            }
-        """)
-        self.minimize_button.clicked.connect(self.parent().showMinimized)
-
-        self.layout.addWidget(self.title_label, 1, Qt.AlignLeft)
-        self.layout.addWidget(self.minimize_button, 0, Qt.AlignRight)
-        self.layout.addWidget(self.close_button, 0, Qt.AlignRight)
-
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            self.drag_start_position = event.globalPos() - self.parent().pos()
-            event.accept()
-
-    def mouseMoveEvent(self, event):
-        if event.buttons() == Qt.LeftButton and hasattr(self, 'drag_start_position'):
-            self.parent().move(event.globalPos() - self.drag_start_position)
-            event.accept()
-
-    def mouseReleaseEvent(self, event):
-        if event.button() == Qt.LeftButton and hasattr(self, 'drag_start_position'):
-            del self.drag_start_position
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing)
-        painter.setPen(QPen(Qt.black, 1))
-        painter.drawRect(0, 0, self.width() - 1, self.height() - 1)
-        painter.end()
-
 class OptionsMenu(QFrame):
     def __init__(self, crosshair):
         super().__init__()
@@ -198,54 +127,46 @@ class OptionsMenu(QFrame):
         self.initUI()
 
     def initUI(self):
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(2, 2, 2, 2)
+
         self.setWindowTitle("BOMBA MENU")
         self.setGeometry(100, 100, 300, 200)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
 
-        # Main layout
-        layout = QVBoxLayout(self)
-        layout.setContentsMargins(2, 2, 2, 2)
-
-        # Title Bar
         self.title_bar = TitleBar(self)
         layout.addWidget(self.title_bar)
 
-        # Apply Button
         self.apply_button = QPushButton("Apply")
         self.apply_button.clicked.connect(self.apply_changes)
         self.apply_button.setStyleSheet(
             "QPushButton { background-color: #505954; color: #fff; border: none; border-radius: 5px; padding: 8px; min-width: 50px; } QPushButton:hover { background-color: #303532; }"
         )
 
-        # Night/Day Theme Button
         self.night_day_button = QPushButton("Night")
         self.night_day_button.clicked.connect(self.toggle_night_day_theme)
         self.night_day_button.setStyleSheet(
             "QPushButton { background-color: #A1B3A8; color: #fff; border: none; border-radius: 5px; padding: 8px; min-width: 80px; } QPushButton:hover { background-color: #808f86; }"
         )
 
-       # Crosshair Diameter
         diameter_label = QLabel("Bomba the diameter of the crosshair in pixels:")
         self.diameter_input = QLineEdit(str(self.crosshair.diameter))
         self.diameter_input.setStyleSheet(
             "QLineEdit { background-color: #f0f0f0; color: #333; border: 1px solid #ccc; border-radius: 5px; padding: 5px; }"
         )
 
-        # Advanced Options Button
         self.advanced_options_button = QPushButton("Advanced Options")
         self.advanced_options_button.clicked.connect(self.open_advanced_options)
         self.advanced_options_button.setStyleSheet(
             "QPushButton { background-color: #707d75; color: #fff; border: none; border-radius: 5px; padding: 8px; min-width: 100px; } QPushButton:hover { background-color: #606b64; }"
         )
 
-        # Vertical Adjust
         vertical_adjust_label = QLabel("Adjust the crosshair position in bomba pixels")
         self.vertical_adjust_input = QLineEdit(str(0))
         self.vertical_adjust_input.setStyleSheet(
             "QLineEdit { background-color: #f0f0f0; color: #333; border: 1px solid #ccc; border-radius: 5px; padding: 5px; }"
         )
 
-        # Transparency Slider
         alpha_label = QLabel("Hide your bomba? (0 to 1)")
         self.alpha_slider = QSlider(Qt.Horizontal)
         self.alpha_slider.setMinimum(0)
@@ -256,12 +177,10 @@ class OptionsMenu(QFrame):
             "QSlider::groove:horizontal { height: 6px; background: #ccc; border-radius: 3px; } QSlider::handle:horizontal { width: 16px; height: 16px; margin: -5px 0; background: #a5aad9; border: 2px solid #a5aad9; border-radius: 8px; }"
         )
 
-        # Create a horizontal layout for the buttons
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.night_day_button)
         button_layout.addWidget(self.apply_button)
 
-        # Add widgets to the layout
         layout.addWidget(self.advanced_options_button)
         layout.addLayout(button_layout)
         layout.addWidget(diameter_label)
@@ -343,6 +262,82 @@ class OptionsMenu(QFrame):
         self.night_day_button.setText("Day")
         self.is_night_theme = True
 
+class TitleBar(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setAutoFillBackground(True)
+
+        self.layout = QHBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+
+        self.title_label = QLabel("BOMBA MENU", self)
+        self.title_label.setStyleSheet("color: black; font-size: 18px; padding-left: 10px;")
+
+        self.close_button = QPushButton("×", self)
+        self.close_button.setStyleSheet("""
+            QPushButton {
+                background-color: transparent;
+                color: black;
+                font-size: 20px;
+                width: 25px;
+                height: 25px;
+                border: none;
+                margin: 0;
+            }
+            QPushButton:hover {
+                border: 1px solid #ccc; 
+                border-radius: 5px;
+                background-color: #dc3545;
+            }
+        """)
+        self.close_button.clicked.connect(self.parent().closeEvent)
+
+        self.minimize_button = QPushButton("–", self)
+        self.minimize_button.setStyleSheet("""
+            QPushButton {
+                background-color: transparent;
+                color: black;
+                font-size: 20px;
+                width: 25px;
+                height: 25px;
+                border: none;
+                margin: 0;
+            }
+            QPushButton:hover {
+                border: 1px solid #ccc; 
+                border-radius: 5px;
+                background-color: #Ffa500
+;
+            }
+        """)
+        self.minimize_button.clicked.connect(self.parent().showMinimized)
+
+        self.layout.addWidget(self.title_label, 1, Qt.AlignLeft)
+        self.layout.addWidget(self.minimize_button, 0, Qt.AlignRight)
+        self.layout.addWidget(self.close_button, 0, Qt.AlignRight)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.drag_start_position = event.globalPos() - self.parent().pos()
+            event.accept()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == Qt.LeftButton and hasattr(self, 'drag_start_position'):
+            self.parent().move(event.globalPos() - self.drag_start_position)
+            event.accept()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton and hasattr(self, 'drag_start_position'):
+            del self.drag_start_position
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(QPen(Qt.black, 1))
+        painter.drawRect(0, 0, self.width() - 1, self.height() - 1)
+        painter.end()
+
 class AdvancedOptionsDialog(QDialog):
     def __init__(self, crosshair):
         super().__init__()
@@ -359,36 +354,30 @@ class AdvancedOptionsDialog(QDialog):
 
         form_layout = QFormLayout()
 
-        # Crosshair Color
         color_label = QLabel("Crosshair Color:")
         self.color_input = QLineEdit("#ffffff")
         form_layout.addRow(color_label, self.color_input)
 
-        # Crosshair Shape
         shape_label = QLabel("Crosshair Shape:")
         self.shape_input = QLineEdit("1-9") 
         form_layout.addRow(shape_label, self.shape_input)
 
         layout.addLayout(form_layout)
 
-       # Add/Remove Outline Border
         self.outline_border_button = QPushButton("Add Outline Border")
         self.outline_border_button.clicked.connect(self.toggle_outline_border)
         layout.addWidget(self.outline_border_button)
 
-        # Apply Button
         apply_button = QPushButton("Apply")
         apply_button.clicked.connect(self.apply_changes)
         layout.addWidget(apply_button)
 
-        # Close Button
         self.close_button = QPushButton("Close")
         self.close_button.clicked.connect(self.close)
         layout.addWidget(self.close_button)
 
         self.setLayout(layout)
 
-        # Apply some styling to make it look cool
         self.setStyleSheet(
             """
             QDialog {
@@ -422,8 +411,6 @@ class AdvancedOptionsDialog(QDialog):
         self.crosshair.set_crosshair_color(color)
         self.crosshair.set_crosshair_shape(shape)
 
-        #self.close()
-
     def toggle_outline_border(self):
         if self.crosshair.is_outline_border:
             self.crosshair.is_outline_border = False
@@ -433,15 +420,12 @@ class AdvancedOptionsDialog(QDialog):
             self.outline_border_button.setText("Remove Outline Border")
         self.crosshair.repaint()
 
-        
 def main():
     app = QApplication(sys.argv)
-
     crosshair_app = CrosshairApp()
-
     options_menu = OptionsMenu(crosshair_app)
     options_menu.show()
-
+    crosshair_app.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
