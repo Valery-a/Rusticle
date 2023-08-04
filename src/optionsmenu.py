d4968f542378c04ffb5cf51c8fc28b914c04700b
Valery-a <ValeryKulchicki@gmail.com>
2023-08-04 16:27:58 +0300

    updating the UI of options

--- src/optionsmenu.py
+++ src/optionsmenu.py
@@ -1,7 +1,6 @@
-from PyQt5.QtWidgets import QSlider, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QFrame
-from PyQt5.QtCore import Qt, QPoint
-import os
-import json
+from PyQt5.QtWidgets import (QSlider, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QFrame)
+from PyQt5.QtCore import (Qt, QPoint)
+import os, json, webbrowser
 
 from advoptions import AdvancedOptionsDialog
 from bar import TitleBar
@@ -17,68 +16,68 @@ class OptionsMenu(QFrame):
 
     def initUI(self):
         layout = QVBoxLayout(self)
-        layout.setContentsMargins(2, 2, 2, 2)
+        layout.setContentsMargins(10, 10, 10, 10)
 
-        self.setWindowTitle("RustOracle")
-        self.setGeometry(100, 100, 300, 200)
+        self.setWindowTitle("WILDOracle++")
+        self.setGeometry(666, 666, 666, 666)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
 
         self.title_bar = TitleBar(self)
         layout.addWidget(self.title_bar)
 
-        self.config_save_button = QPushButton("Config Save")
+        self.config_save_button = QPushButton("╚═ Save Config ═╝")
         self.config_save_button.clicked.connect(self.save_settings)
         self.config_save_button.setStyleSheet(
-            "QPushButton { background-color: #FF9900; color: #000000; border: none; border-radius: 5px; padding: 8px; min-width: 100px; } QPushButton:hover { background-color: #FF6600; }"
+            "QPushButton { background-color: #FF9900; color: #000000; border: none; border-radius: 50px; padding: 10px; min-width: 150px; } QPushButton:hover { background-color: #FF6600; }"
         )
         layout.addWidget(self.config_save_button)
 
-        self.apply_button = QPushButton("Apply")
+        self.apply_button = QPushButton("╚═ Apply Changes ═╝")
         self.apply_button.clicked.connect(self.apply_changes)
         self.apply_button.setStyleSheet(
-            "QPushButton { background-color: #FFA500; color: #000000; border: none; border-radius: 5px; padding: 8px; min-width: 50px; } QPushButton:hover { background-color: #FF6600; }"
+            "QPushButton { background-color: #FFA500; color: #000000; border: none; border-radius: 50px; padding: 10px; min-width: 100px; } QPushButton:hover { background-color: #FF6600; }"
         )
 
-        self.night_day_button = QPushButton("Night")
+        self.night_day_button = QPushButton("☾☼ NIGHT ☼☽")
         self.night_day_button.clicked.connect(self.toggle_night_day_theme)
         self.night_day_button.setStyleSheet(
-            "QPushButton { background-color: #808f86; color: #000000; border: none; border-radius: 5px; padding: 8px; min-width: 80px; } QPushButton:hover { background-color: #707d75; }"
+            "QPushButton { background-color: #808f86; color: #000000; border: none; border-radius: 50px; padding: 10px; min-width: 130px; } QPushButton:hover { background-color: #707d75; }"
         )
 
-        diameter_label = QLabel("Change the diameter of the crosshair in pixels:")
+        diameter_label = QLabel("Δ Change crosshair diameter (in pixels): Δ")
         self.diameter_input = QLineEdit(str(0))
         self.diameter_input.setStyleSheet(
-            "QLineEdit { background-color: #f0f0f0; color: #333; border: 1px solid #ccc; background: #a5aad9; border-radius: 5px; padding: 5px; }"
+            "QLineEdit { background-color: #f0f0f0; color: #333; border: 2px solid #ccc; background: #a5aad9; border-radius: 10px; padding: 15px; }"
         )
 
-        self.advanced_options_button = QPushButton("Advanced Options")
+        self.advanced_options_button = QPushButton("➤ Advanced Options ➤")
         self.advanced_options_button.clicked.connect(self.open_advanced_options)
         self.advanced_options_button.setStyleSheet(
-            "QPushButton { background-color: #707d75; color: #000000; border: none; border-radius: 5px; padding: 8px; min-width: 100px; } QPushButton:hover { background-color: #606b64; }"
+            "QPushButton { background-color: #707d75; color: #000000; border: none; border-radius: 50px; padding: 20px; min-width: 200px; } QPushButton:hover { background-color: #606b64; }"
         )
 
-        vertical_adjust_label = QLabel("Adjust the crosshair position in pixels")
+        vertical_adjust_label = QLabel("♨ Adjust vertical position (in pixels) ♨")
         self.vertical_adjust_input = QLineEdit(str(0))
         self.vertical_adjust_input.setStyleSheet(
-            "QLineEdit { background-color: #f0f0f0; color: #333; border: 1px solid #ccc; background: #a5aad9; border-radius: 5px; padding: 5px; }"
+            "QLineEdit { background-color: #f0f0f0; color: #333; border: 2px solid #ccc; background: #a5aad9; border-radius: 10px; padding: 15px; }"
         )
         layout.addWidget(vertical_adjust_label)
         layout.addWidget(self.vertical_adjust_input)
 
-        alpha_label = QLabel("Adjust the transparency of the crosshair: (0 to 1)")
+        alpha_label = QLabel("▤ Adjust crosshair transparency (0 to 1) ▤")
         self.alpha_slider = QSlider(Qt.Horizontal)
         self.alpha_slider.setMinimum(0)
         self.alpha_slider.setMaximum(100)
         self.alpha_slider.setValue(int(self.crosshair.alpha * 100))
         self.alpha_slider.valueChanged.connect(self.update_alpha)
         self.alpha_slider.setStyleSheet(
-            "QSlider::groove:horizontal { height: 6px; background: #ccc; border-radius: 3px; } QSlider::handle:horizontal { width: 16px; height: 16px; margin: -5px 0; background: #a5aad9; border: 2px solid #a5aad9; border-radius: 8px; }"
+            "QSlider::groove:horizontal { height: 15px; background: #ccc; border-radius: 7px; } QSlider::handle:horizontal { width: 30px; height: 30px; margin: -5px 0; background: #a5aad9; border: 5px solid #a5aad9; border-radius: 20px; }"
         )
 
-        self.calculator_button = QPushButton("CALCULATOR")
+        self.calculator_button = QPushButton("♢ CALCULATOR ♢")
         self.calculator_button.clicked.connect(self.open_calculator)
         self.calculator_button.setStyleSheet(
-            "QPushButton { background-color: #70ad47; color: #000000; border: none; border-radius: 5px; padding: 8px; min-width: 100px; } QPushButton:hover { background-color: #548235; }"
+            "QPushButton { background-color: #70ad47; color: #000000; border: none; border-radius: 50px; padding: 20px; min-width: 200px; } QPushButton:hover { background-color: #548235; }"
         )
 
         button_layout = QHBoxLayout()
@@ -102,7 +101,7 @@ class OptionsMenu(QFrame):
             """
             OptionsMenu {
                 background-color: #f5f5f5;
-                border: 1px solid #ccc;
+                border: 3px solid #ccc;
             }
             QLabel, QLineEdit {
                 color: #333;
@@ -122,29 +121,30 @@ class OptionsMenu(QFrame):
         calculator_dialog.exec_()
 
     def save_settings(self):
-        # Gather the settings to save
+        # Save the settings to a configuration file (e.g., config.json)
         settings = {
             "diameter": self.crosshair.diameter,
             "alpha": self.crosshair.alpha,
             "crosshair_color": self.crosshair.crosshair_color.name(),
             "crosshair_shape": self.crosshair.crosshair_shape,
             "is_outline_border": self.crosshair.is_outline_border,
-            "night_theme": self.is_night_theme,  # Save the current theme state
-            "vertical_adjust_input": int(self.vertical_adjust_input.text()),  # Save the vertical adjust value
+            "night_theme": self.is_night_theme,
+            "vertical_adjust_input": int(self.vertical_adjust_input.text()),
         }
 
-        # Save the settings to a configuration file (e.g., config.json)
-        config_folder = "config"
-        os.makedirs(config_folder, exist_ok=True)  # Create the config folder if it doesn't exist
-        config_path = os.path.join(config_folder, "config.json")
+        config_folder = "c0nF1g"
+        os.makedirs(config_folder, exist_ok=True)
+        config_path = os.path.join(config_folder, "s3cr3t_c0nF1g.json")
 
         with open(config_path, "w") as config_file:
-            json.dump(settings, config_file)
+            json.dump(settings, config_file, indent=2)
+
+        webbrowser.open_new_tab("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
     def load_settings(self):
         # Load settings from the configuration file (if available)
-        config_folder = "config"
-        config_path = os.path.join(config_folder, "config.json")
+        config_folder = "c0nF1g"
+        config_path = os.path.join(config_folder, "s3cr3t_c0nF1g.json")
 
         try:
             with open(config_path, "r") as config_file:
@@ -157,7 +157,7 @@ class OptionsMenu(QFrame):
             self.crosshair.set_crosshair_color(settings.get("crosshair_color", self.crosshair.crosshair_color.name()))
             self.crosshair.set_crosshair_shape(settings.get("crosshair_shape", self.crosshair.crosshair_shape))
             self.crosshair.is_outline_border = settings.get("is_outline_border", self.crosshair.is_outline_border)
-            self.crosshair.new_window_y += settings.get("vertical_adjust_input", 0)  # Apply vertical adjust value
+            self.crosshair.new_window_y += settings.get("vertical_adjust_input", 0)
             self.crosshair.setGeometry(self.crosshair.new_window_x, self.crosshair.new_window_y,
                                        self.crosshair.window_width, self.crosshair.window_height)
             self.crosshair.repaint()
@@ -192,8 +192,8 @@ class OptionsMenu(QFrame):
     def open_advanced_options(self):
         dialog = AdvancedOptionsDialog(self.crosshair)
         options_menu_position = self.mapToGlobal(QPoint(0, 0))
-        x_offset = self.width() + 10
-        y_offset = 0
+        x_offset = self.width() + 100
+        y_offset = 100
 
         dialog.move(options_menu_position + QPoint(x_offset, y_offset))
         dialog.setModal(True)
@@ -214,12 +214,12 @@ class OptionsMenu(QFrame):
         self.setStyleSheet(
             "OptionsMenu { background-color: #f5f5f5; } QLabel, QLineEdit { color: #333; } QPushButton { background-color: #007BFF; color: #fff; }"
         )
-        self.night_day_button.setText("Night")
+        self.night_day_button.setText("☾☼ NIGHT ☼☽")
         self.is_night_theme = False
 
     def set_night_theme(self):
         self.setStyleSheet(
             "OptionsMenu { background-color: #303030; } QLabel, QLineEdit { color: #f0f0f0; } QPushButton { background-color: #212121; color: #fff; }"
         )
-        self.night_day_button.setText("Day")
+        self.night_day_button.setText("☼☾ DAY ☾☼")
         self.is_night_theme = True
